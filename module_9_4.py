4cbd9aabf9f2a35d074d6df9cd731000503205cf
Taifun2005 <lider2005@yandex.ru>
2024-10-15 15:49:56 +0700

    Add files via upload

--- module_9_4.py
+++ module_9_4.py
@@ -1,5 +1,3 @@
-from asyncore import write
-
 first = 'Мама мыла раму'
 second = 'Рамена мало бы ло'
 
@@ -11,12 +9,28 @@ def get_advanced_writer(file_name):
     def write_everything(*data_set):
         file = open(file_name, 'w', encoding='utf-8')
         for i in data_set:
-            file.write(i)
+            # str1 = str(i)
+            file.write(str(i))
+            file.write('\n')
         file.close()
-    def get_advanced_writer():
-        return write_everything
+    # def get_advanced_writer():
+    return write_everything
 
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+from random import choice
+
+class MysticBall():
+    def __init__(self, *words):
+        self.words = words      # хранящий коллекцию строк.
+
+    def __call__(self):
+        return choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
 
